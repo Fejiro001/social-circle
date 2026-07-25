8e1c48e5b0c07c0861832bedf199b12e638dafff
Fejiro001 <aberefejiro@gmail.com>
2026-07-25 16:14:41 -0500

    Added PostRepo and UserFollowRepo classes to encapsulate CRUD operations and queries for posts and user follow relationships.

--- SocialCircle.DAL/PostRepo.cs
+++ SocialCircle.DAL/PostRepo.cs
@@ -1,6 +1,44 @@
-﻿namespace SocialCircle.DAL
+﻿using SocialCircle.Models;
+
+namespace SocialCircle.DAL
 {
     public class PostRepo
     {
+        private readonly SocialCircleDbContext _context;
+        public PostRepo(SocialCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Post> GetFeedPosts(int currentUserId)
+        {
+            return _context.Posts.OrderByDescending(p => p.Timestamp).ToList();
+        }
+
+        public List<Post> GetUsersPosts(int targetUserId)
+        {
+            return _context.Posts.Where(p => p.UserId == targetUserId).ToList();
+        }
+
+        public void AddPost(Post post)
+        {
+            _context.Posts.Add(post);
+        }
+
+        public void UpdatePost(Post post)
+        {
+            _context.Posts.Update(post);
+        }
+
+        public void DeletePost(int postId)
+        {
+            Post post = _context.Posts.Find(postId);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+            }
+        }
+
+        public void Save() => _context.SaveChanges();
     }
 }

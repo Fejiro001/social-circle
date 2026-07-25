6015c9f5e5529f3ef0a5acdf36f5fa8140fb0a15
Fejiro001 <aberefejiro@gmail.com>
2026-07-25 15:27:57 -0500

    Introduced dependency injection and repository pattern for user management. Added UserRepo for database access and UserService for business logic. Implemented UserController with endpoints for listing users and viewing profiles.

--- SocialCircle.BLL/UserService.cs
+++ SocialCircle.BLL/UserService.cs
@@ -1,6 +1,18 @@
-﻿namespace SocialCircle.BLL
+﻿using SocialCircle.DAL;
+using SocialCircle.Models;
+
+namespace SocialCircle.BLL
 {
     public class UserService
     {
+        private readonly UserRepo _repo;
+        public UserService(UserRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public List<User> GetAllUsers() => _repo.GetAllUsers();
+
+        public User GetUserById(int userId) => _repo.GetUserById(userId);
     }
 }
---
+++ SocialCircle/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialCircle.BLL;
+using SocialCircle.Models;
+
+namespace SocialCircle.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+        public UserController(UserService service)
+        {
+            _userService = service;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<User> users = _userService.GetAllUsers();
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult UsersProfile(int id)
+        {
+            User user = _userService.GetUserById(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+    }
+}

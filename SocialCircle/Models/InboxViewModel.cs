e32d9e2094b04a480911f5d89ec555653de97f7a
patrick rukundo <prukundo@mail.com>
2026-07-26 09:08:13 -0500

    inbox view model added

---
+++ SocialCircle/Models/InboxViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialCircle.Models
+{
+    public class InboxViewModel
+    {
+        public int TargetUserId { get; set; }
+        public string TargetUserName { get; set; } = string.Empty;
+        public string TargetAvatar { get; set; } = string.Empty;
+        public string LastMessageText { get; set; } = string.Empty;
+        public DateTime LastMessageTimestamp { get; set; }
+        public bool IsUnread { get; set; }
+    }
+}

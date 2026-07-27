05538653d25fcb4a22162828b05811760c13c255
patrick rukundo <prukundo@mail.com>
2026-07-27 00:08:00 -0500

    direct message initial dmrepo set up

--- SocialCircle.BLL/DirectMessageService.cs
+++ SocialCircle.BLL/DirectMessageService.cs
@@ -1,6 +1,18 @@
-﻿namespace SocialCircle.BLL
+﻿using SocialCircle.DAL;
+using SocialCircle.Models;
+
+namespace SocialCircle.BLL
 {
     public class DirectMessageService
     {
+        private readonly DirectMessageRepo _dmRepo;
+
+        public DirectMessageService(DirectMessageRepo dmRepo)
+        {
+            _dmRepo = dmRepo;
+        }
+
+
     }
+
 }

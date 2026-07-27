b1794b0eacd284982dcf7b8ebe5074eb50cb256a
patrick rukundo <prukundo@mail.com>
2026-07-26 23:23:51 -0500

    story repo logic added

--- SocialCircle.DAL/StoryRepo.cs
+++ SocialCircle.DAL/StoryRepo.cs
@@ -1,6 +1,25 @@
-﻿namespace SocialCircle.DAL
+﻿using SocialCircle.Models;
+
+namespace SocialCircle.DAL
 {
     public class StoryRepo
     {
+        private readonly SocialCircleDbContext _context;
+
+        public StoryRepo(SocialCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddStory(Story story)
+        {
+            _context.Stories.Add(story);
+            _context.SaveChanges();
+        }
+
+        public List<Story> FetchActiveStories()
+        {
+            return _context.Stories.ToList();
+        }
     }
 }

95e293cb690c06ebfb1c5e51e25a9c379e1ac1ab
patrick rukundo <prukundo@mail.com>
2026-07-26 23:28:19 -0500

    direct message fetch code added

--- SocialCircle.DAL/DirectMessageRepo.cs
+++ SocialCircle.DAL/DirectMessageRepo.cs
@@ -17,6 +17,19 @@ namespace SocialCircle.DAL
             _context.SaveChanges();
         }
 
+        public List<DirectMessage> FetchConversation(int userA, int userB)
+        {
+            return _context.DirectMessages
+                .Where(m => (m.SenderId == userA && m.ReceiverId == userB) ||
+                            (m.SenderId == userB && m.ReceiverId == userA))
+                .ToList();
+        }
 
+        public List<DirectMessage> FetchAllUserMessages(int userId)
+        {
+            return _context.DirectMessages
+                .Where(m => m.SenderId == userId || m.ReceiverId == userId)
+                .ToList();
+        }
     }
 }

af2c23e0ffe7be92f38f9cb45f9be762eb332d9c
patrick rukundo <prukundo@mail.com>
2026-07-27 00:19:13 -0500

    record(save) a story logic with storyview object creation

--- SocialCircle.BLL/StoryViewService.cs
+++ SocialCircle.BLL/StoryViewService.cs
@@ -12,6 +12,25 @@ namespace SocialCircle.BLL
             _storyViewRepo = storyViewRepo;
         }
 
-       
+        public void RecordStoryView(int storyId, int userId)
+        {
+            // Verify if this user has already logged a view for this specific story
+            var existingViews = _storyViewRepo.FetchStoryViewers(storyId);
+            bool alreadyViewed = existingViews.Any(v => v.UserId == userId);
+
+            if (!alreadyViewed)
+            {
+                var newView = new StoryView
+                {
+                    StoryId = storyId,
+                    UserId = userId,
+                    ViewDateTime = DateTime.Now
+                };
+
+                _storyViewRepo.InsertStoryView(newView);
+            }
+        }
+
+  
     }
 }

76f4f3dda702a4b9d0103393c2a726323472b62e
Ivan <ivanchijindu@gmail.com>
2026-07-27 10:35:46 -0500

    initial commits

--- SocialCircle.BLL/CommentService.cs
+++ SocialCircle.BLL/CommentService.cs
@@ -1,6 +1,45 @@
-﻿namespace SocialCircle.BLL
+﻿using SocialCircle.DAL;
+using SocialCircle.Models;
+
+namespace SocialCircle.BLL
 {
     public class CommentService
     {
+        private readonly CommentRepo _commentRepo;
+
+        public CommentService(CommentRepo commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        public List<Comment> GetPostComments(int postId)
+        {
+            return _commentRepo.FetchCommentsForPost(postId);
+        }
+
+        public void AddComment(int postId, string commentText)
+        {
+            if (string.IsNullOrWhiteSpace(commentText))
+            {
+                throw new ArgumentException("Comment cannot be empty.");
+            }
+
+            Comment comment = new Comment
+            {
+                PostId = postId,
+                UserId = 1, // Current logged-in user
+                CommentText = commentText,
+                Timestamp = DateTime.Now
+            };
+
+            _commentRepo.InsertComment(comment);
+            _commentRepo.Save();
+        }
+
+        public void RemoveComment(int commentId)
+        {
+            _commentRepo.DeleteComment(commentId);
+            _commentRepo.Save();
+        }
     }
 }
--- SocialCircle.DAL/PostLikeRepo.cs
+++ SocialCircle.DAL/PostLikeRepo.cs
@@ -1,6 +1,46 @@
-﻿namespace SocialCircle.DAL
+﻿using SocialCircle.Models;
+
+namespace SocialCircle.DAL
 {
     public class PostLikeRepo
     {
+        private readonly SocialCircleDbContext _context;
+
+        public PostLikeRepo(SocialCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public void InsertLike(PostLike like)
+        {
+            _context.PostLikes.Add(like);
+        }
+
+        public void DeleteLike(int userId, int postId)
+        {
+            PostLike like = _context.PostLikes.Find(userId, postId);
+
+            if (like != null)
+            {
+                _context.PostLikes.Remove(like);
+            }
+        }
+
+        public bool CheckIfUserLiked(int userId, int postId)
+        {
+            return _context.PostLikes.Any(l =>
+                l.UserId == userId &&
+                l.PostId == postId);
+        }
+
+        public int GetTotalLikes(int postId)
+        {
+            return _context.PostLikes.Count(l => l.PostId == postId);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
     }
 }

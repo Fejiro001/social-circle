5b8070fd1df7d172f22fa6db8e7f7de93e049f3e
patrick rukundo <prukundo@mail.com>
2026-07-26 23:27:32 -0500

    direct message repo, basic context code added

--- SocialCircle.DAL/DirectMessageRepo.cs
+++ SocialCircle.DAL/DirectMessageRepo.cs
@@ -1,6 +1,22 @@
-﻿namespace SocialCircle.DAL
+﻿using SocialCircle.Models;
+
+namespace SocialCircle.DAL
 {
     public class DirectMessageRepo
     {
+        private readonly SocialCircleDbContext _context;
+
+        public DirectMessageRepo(SocialCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public void InsertMessage(DirectMessage message)
+        {
+            _context.DirectMessages.Add(message);
+            _context.SaveChanges();
+        }
+
+
     }
 }

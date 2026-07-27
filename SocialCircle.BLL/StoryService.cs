9ece73636595cad0e2a0d9a95fdaa8090aad17fe
patrick rukundo <prukundo@mail.com>
2026-07-26 23:48:09 -0500

    story service initial code declaration

--- SocialCircle.BLL/StoryService.cs
+++ SocialCircle.BLL/StoryService.cs
@@ -1,6 +1,19 @@
-﻿namespace SocialCircle.BLL
+﻿using SocialCircle.DAL;
+using SocialCircle.Models;
+
+namespace SocialCircle.BLL
 {
     public class StoryService
     {
+        private readonly StoryRepo _storyRepo;
+        private readonly StoryViewRepo _viewRepo;
+
+        public StoryService(StoryRepo storyRepo, StoryViewRepo viewRepo)
+        {
+            _storyRepo = storyRepo;
+            _viewRepo = viewRepo;
+        }
+
+
     }
 }

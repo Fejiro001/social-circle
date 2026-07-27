84f8bf6d5316313c9e9457fa4add3bb7675812ab
patrick rukundo <prukundo@mail.com>
2026-07-27 08:15:59 -0500

    initial direct message controller layout

---
+++ SocialCircle/Controllers/DirectMessageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialCircle.BLL;
+using SocialCircle.Models;
+
+namespace SocialCircle.Controllers
+{
+    public class DirectMessageController : Controller
+    {
+        private readonly DirectMessageService _dmService;
+        private const int CurrentUserMockID = 1; 
+
+        public DirectMessageController(DirectMessageService dmService)
+        {
+            _dmService = dmService;
+        }
+
+
+    }
+}

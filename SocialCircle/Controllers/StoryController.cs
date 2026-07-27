c36dec812c24ccd0ba0b5ae2819609f15e616e41
patrick rukundo <prukundo@mail.com>
2026-07-27 01:17:16 -0500

    story control initial code layout

---
+++ SocialCircle/Controllers/StoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialCircle.BLL;
+using SocialCircle.Models;
+
+namespace SocialCircle.Controllers
+{
+    public class StoryController : Controller
+    {
+        private readonly StoryService _storyService;
+        private readonly StoryViewService _storyViewService;
+        private const int CurrentUserMockID = 1;
+
+        public StoryController(StoryService storyService, StoryViewService storyViewService)
+        {
+            _storyService = storyService;
+            _storyViewService = storyViewService;
+        }
+
+       
+    }
+}

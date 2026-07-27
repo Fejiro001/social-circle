201483676d55c550f42794255ace2c1aa20af1f5
patrick rukundo <prukundo@mail.com>
2026-07-26 23:25:30 -0500

    story view repo code being added

--- SocialCircle.DAL/StoryViewRepo.cs
+++ SocialCircle.DAL/StoryViewRepo.cs
@@ -1,6 +1,25 @@
-﻿namespace SocialCircle.DAL
+﻿using SocialCircle.Models;
+
+namespace SocialCircle.DAL
 {
     public class StoryViewRepo
     {
+        private readonly SocialCircleDbContext _context;
+
+        public StoryViewRepo(SocialCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public void InsertStoryView(StoryView view)
+        {
+            _context.StoryViews.Add(view);
+            _context.SaveChanges();
+        }
+
+        public List<StoryView> FetchStoryViewers(int storyId)
+        {
+            return _context.StoryViews.Where(v => v.StoryId == storyId).ToList();
+        }
     }
 }

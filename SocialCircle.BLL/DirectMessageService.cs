0ab6ee3da5e81eca5cd2248c55c5ed46fb964596
patrick rukundo <prukundo@mail.com>
2026-07-27 00:08:46 -0500

    send chat logic added with new message object

--- SocialCircle.BLL/DirectMessageService.cs
+++ SocialCircle.BLL/DirectMessageService.cs
@@ -12,6 +12,19 @@ namespace SocialCircle.BLL
             _dmRepo = dmRepo;
         }
 
+        public void SendChatMessage(int senderId, int receiverId, string text)
+        {
+            var message = new DirectMessage
+            {
+                SenderId = senderId,
+                ReceiverId = receiverId,
+                MessageText = text,
+                Timestamp = DateTime.Now,
+                IsRead = false
+            };
+            _dmRepo.InsertMessage(message);
+        }
+
 
     }
 

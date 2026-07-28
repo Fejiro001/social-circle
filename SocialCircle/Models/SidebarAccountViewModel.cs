4c70ca6c11d6016d3290f60fb678c2981650150d
Fejiro001 <aberefejiro@gmail.com>
2026-07-28 08:51:20 -0500

    Added SidebarAccountViewModel and SidebarAccountViewComponent to render the sidebar account section dynamically with current user data. Replace static markup in _AppLayout.cshtml with the new component. Add Default.cshtml view for the component to display user info.

---
+++ SocialCircle/Models/SidebarAccountViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SocialCircle.Models
+{
+    public class SidebarAccountViewModel
+    {
+        public string DisplayName { get; set; } = "My Account";
+        public string Handle { get; set; } = "@user";
+        public string? ProfilePicUrl { get; set; }
+        public string Initial { get; set; } = "A";
+    }
+}
---
+++ SocialCircle/ViewComponents/SidebarAccountViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialCircle.BLL;
+using SocialCircle.Helpers;
+using SocialCircle.Models;
+
+namespace SocialCircle.ViewComponents
+{
+    public class SidebarAccountViewComponent : ViewComponent
+    {
+        private readonly UserService _userService;
+        public SidebarAccountViewComponent(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            int currentUserId = CurrentUser.Id;
+            var user = _userService.GetUserById(currentUserId);
+
+            var vm = new SidebarAccountViewModel
+            {
+                DisplayName = user.UserName,
+                Handle = $"@{user.UserName}",
+                ProfilePicUrl = user.ProfilePicUrl,
+                Initial = user.UserName.Substring(0, 1).ToUpper()
+            };
+
+            return View(vm);
+        }
+    }
+}
